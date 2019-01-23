71555d2f254bd67907ab44599b8a385e33ba4261
qarajesh2171 <raj_dixit1992@rediffmail.com>
2019-01-23 15:39:25 +0530

    Commit 3

--- Scripts/New Test Case 1/Script1548219772025.groovy
+++ Scripts/New Test Case 1/Script1548219772025.groovy
@@ -13,3 +13,9 @@ import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import internal.GlobalVariable as GlobalVariable
 
+WebUI.openBrowser('www.greendesk.in')
+
+WebUI.verifyTextPresent('Healthy Greens On Your', false)
+
+WebUI.closeBrowser()
+
---
+++ Test Suites/New Test Suite 1.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite 1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>f1360527-60cf-4f32-b7f9-ce2f629b6513</testSuiteGuid>
+   <testCaseLink>
+      <guid>53563777-876a-4bc0-b6ad-7077293ff04d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/New Test Case 1</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
